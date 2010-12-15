94a73e7a84da899c77e39459603606d734db906f
Le Van Xung <xunglv@gmail.com>
2010-12-15 23:21:50 +0700

    added convert database tool

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,6 @@
 #require "./sockets/udppunch.rb"
 #require "./twitter/twitter.rb"
 #require "./xml/testxml.rb"
-require "./tools/DictImporter/DictImporter.rb"
+#require "./tools/DictImporter/DictImporter.rb"
+require "./tools/ratp-converter/ratp.rb"
 
